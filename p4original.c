dace1109f9465b922f3d2e7bb08b3a6421c25dfe
purplesnow31 <94883115+purplesnow31@users.noreply.github.com>
2022-02-02 10:01:40 +0000

    done

---
+++ p4original.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int input()
+{
+int a;
+printf("Enter a number\n");
+scanf("%d", &a);
+return a;
+}
+void input_array(int n, int a[n])
+{
+  for(int i=0;i<n;i++)
+  {
+    printf("Enter the element no %d of the array\n",i);
+    scanf("%d",&a[i]);
+  }
+}
+
+int array_sum(int n, int a[n])
+{
+  int sum=0;
+  for(int i=0;i<n;i++)
+  {
+    sum += a[i];
+  }
+  return sum;
+}
+int output(int n, int a[n], int sum)
+{
+  int i;
+  printf("The sum of");
+  for(i=0;i<n-1;i++)
+  {
+    printf("%d + ",a[i]);
+  }
+  printf("%d = %d\n", a[i], sum);
+}
+int main()
+{
+  int n,sum;
+  n=input();
+  int a[n];
+  input_array(n,a);
+  sum=array_sum(n,a);
+  output(n,a,sum);
+}
